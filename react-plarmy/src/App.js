5da31fc50ba06ce3d7066dd0c0a8630bb2cf6ce7
atmavi <atmavi0001@gmail.com>
2020-06-22 12:44:04 +0800

    WIP-- cleapup landing page

--- react-plarmy/src/App.js
+++ react-plarmy/src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 import './css/Style.css';
 
 import Navbar from './components/Navs/Navbar';
+import Landing from './pages/Landing';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Landing />
+      <Footer />
     </div>
   );
 }
--- react-plarmy/src/components/Navs/Navbar.jsx
+++ react-plarmy/src/components/Navs/Navbar.jsx
@@ -4,9 +4,9 @@ import UserNav from './UserNav';
 
 function NavBar() {
    return (
-      <nav class="nav">
-         <a href="/" class="nav__logo">Gulaid</a>
-         <input type="text" name="nav__search-box" id="nav__search-box" class="nav__search-box" />
+      <nav className="nav">
+         <a href="/" className="nav__logo">Plarmy</a>
+         <input type="text" name="nav__search-box" id="nav__search-box" className="nav__search-box" />
          <UserNav />
       </nav>
    )
