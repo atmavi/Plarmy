9e619f06d6977b5890151c3d897a80c2a100e44e
atmavi <atmavi0001@gmail.com>
2020-07-03 22:54:16 +0800

    Completed Product page

---
+++ react-plarmy/src/pages/Products.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+import { getAllProducts } from '../services/products';
+
+import Product from '../components/Product';
+
+const Products = () => {
+   const [products, setProducts] = useState([]);
+
+   useEffect(() => {
+      getAllProducts()
+         .then(data => setProducts(data))
+   })
+
+   return (
+      <div className="products">
+         {products.map(product =>
+            (<Product product={product} key={product.id} />)
+         )}
+      </div>
+   );
+}
+
+export default Products;   
