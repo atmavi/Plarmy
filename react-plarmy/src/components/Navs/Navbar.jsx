cf477fb80192b81dc6619d4db7b249b4ac85d95a
atmavi <atmavi0001@gmail.com>
2020-07-04 22:23:28 +0800

    Replaced a tag to Link component

--- react-plarmy/src/components/Navs/Navbar.jsx
+++ react-plarmy/src/components/Navs/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import UserNav from './UserNav';
 
 function NavBar() {
    return (
       <nav className="nav">
-         <a href="/" className="nav__logo">Plarmy</a>
+         <Link to="/" className="nav__logo">Plarmy</Link>
+         {/* <a href="/" className="nav__logo">Plarmy</a> */}
          <input type="text" name="nav__search-box" id="nav__search-box" className="nav__search-box" />
          <UserNav />
       </nav>
