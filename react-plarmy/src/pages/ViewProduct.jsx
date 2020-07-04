7cda7f21907f8b7628bb04d85b3580abe0591e0a
atmavi <atmavi0001@gmail.com>
2020-07-05 03:16:29 +0800

    Completed v1 of ViewProduct component

--- react-plarmy/src/pages/ViewProduct.jsx
+++ react-plarmy/src/pages/ViewProduct.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import QuantityBtns from '../components/QuatityBtns';
 
 const ViewProduct = () => {
-   let { id } = useParams();
-   console.log(id)
+   // let { id } = useParams();
    return (
-      <div className="ViewProduct">
-         <h1>Product ID: {id}</h1>
-         This is view product page
+      <div className="view-product">
+         <div className="view-product__img-wrapper">
+            <img
+               src="https://images.pexels.com/photos/144248/potatoes-vegetables-erdfrucht-bio-144248.jpeg?auto=compress&cs=tinysrgb&h=350"
+               alt="Potato"
+               className="view-product__img"
+            />
+         </div>
+         <div className="view-product__details">
+            <h2 className="view-product__name">Potatoes</h2>
+            <span className="view-product__per-volume">Per kilo</span>
+            <p className="view-product__description">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia praesentium quia soluta atque? Vitae facere placeat maxime, odit quasi, tenetur laborum debitis commodi ut fugit quod, aperiam nam fugiat illum. Laudantium in consectetur quis odio nisi totam vel voluptas molestias!
+            </p>
+            <h3 className="view-product__price">Php 300.00</h3>
+            <div className="view-product__quantity-box">
+               <span className="view-product__label">quantity:</span>
+               <QuantityBtns />
+            </div>
+            <span className="view-product__warning">
+               * Picture/s may vary from actual product.
+            </span>
+         </div>
       </div>
    );
 }
