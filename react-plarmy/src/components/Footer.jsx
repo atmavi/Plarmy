d07cba1a9102ce2cdb0d5a4585b44d318e505a1a
atmavi <atmavi0001@gmail.com>
2020-07-16 22:43:38 +0800

    WIP adding contents of the footer

--- react-plarmy/src/components/Footer.jsx
+++ react-plarmy/src/components/Footer.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 function Footer() {
    return (
       <footer className="footer">
-         this is the footer
+         <div className="footer__logo">
+            Plarmy
+         </div>
+
+         <div class="footer__navigation">
+            <ul class="footer__list">
+               <li class="footer__item"><a href="#" class="footer__link">Company</a></li>
+               <li class="footer__item"><a href="#" class="footer__link">Contact us</a></li>
+               <li class="footer__item"><a href="#" class="footer__link">Carrers</a></li>
+               <li class="footer__item"><a href="#" class="footer__link">Privacy policy</a></li>
+               <li class="footer__item"><a href="#" class="footer__link">Terms</a></li>
+            </ul>
+         </div>
       </footer>
    )
 }
