170cb4397754fec66895a978bd56c3d051b12c76
atmavi <atmavi0001@gmail.com>
2020-07-24 05:41:10 +0800

    Added getProduct function from product service

--- react-plarmy/src/services/products.js
+++ react-plarmy/src/services/products.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
 
+const url = 'http://192.168.254.105:4000/api';
+
 export const getAllProducts = () => {
-   const res = axios.get('http://192.168.254.105:4000/api/products')
+   const res = axios.get(url + '/products')
       .then(res => res.data)
       .catch(err => err)
    return res;
 }
+
+export const getProduct = id => {
+   console.log(id)
+   const res = axios.get(url + '/products/' + id)
+      .then(res => res.data)
+      .catch(err => err)
+   return res;
+}
+
 
 
