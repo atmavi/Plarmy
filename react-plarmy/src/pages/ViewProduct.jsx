8f0539e553585ecadcd078a6dc1a75540d41e4b7
atmavi <atmavi0001@gmail.com>
2020-07-24 05:42:21 +0800

    ViewProduct component now uses fetched data from db

--- react-plarmy/src/pages/ViewProduct.jsx
+++ react-plarmy/src/pages/ViewProduct.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+import { getProduct } from '../services/products';
 import QuantityBtns from '../components/QuatityBtns';
 
+
 const ViewProduct = () => {
-   // let { id } = useParams();
+   let { id } = useParams();
+
+   const [product, setProduct] = useState({});
+
+   useEffect(() => {
+      getProduct(id)
+         .then(data => setProduct(data))
+   }, [])
+
+   const { name, price, sellVolume, image } = product;
+
    return (
       <div className="view-product">
          <div className="view-product__img-wrapper">
             <img
-               src="https://images.pexels.com/photos/144248/potatoes-vegetables-erdfrucht-bio-144248.jpeg?auto=compress&cs=tinysrgb&h=350"
-               alt="Potato"
+               alt={name}
+               src={image}
                className="view-product__img"
             />
          </div>
          <div className="view-product__details">
-            <h2 className="view-product__name">Potatoes</h2>
-            <span className="view-product__per-volume">Per kilo</span>
+            <h2 className="view-product__name">{name}</h2>
+            <span className="view-product__per-volume">{sellVolume}</span>
             <p className="view-product__description">
                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia praesentium quia soluta atque? Vitae facere placeat maxime, odit quasi, tenetur laborum debitis commodi ut fugit quod, aperiam nam fugiat illum. Laudantium in consectetur quis odio nisi totam vel voluptas molestias!
             </p>
-            <h3 className="view-product__price">Php 300.00</h3>
+            <h3 className="view-product__price">Php {price}</h3>
             <div className="view-product__quantity-box">
                <span className="view-product__label">quantity:</span>
                <QuantityBtns />
