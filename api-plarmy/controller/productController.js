a1a8e1029ebe3b504362c98fe0825004eef20b3a
atmavi <atmavi0001@gmail.com>
2020-07-25 08:46:28 +0800

    Added product controller

---
+++ api-plarmy/controller/productController.js
@@ -0,0 +1,74 @@
+const Product = require('../model/product');
+
+
+exports.home = (req, res) => {
+   res.redirect('/api/products');
+};
+
+exports.list = (req, res) => {
+   Product.find({}, (err, products) => {
+      if (err) {
+         res.json("Something went wrong");
+      } else {
+         res.json(products);
+      }
+   });
+};
+
+exports.add = (req, res) => {
+   Product.create(
+      {
+         name: 'Orange',
+         price: 100,
+         sellVolume: 'per kilo',
+         image: './img/products/orange.png',
+         author: 'Me',
+         type: 'fruit',
+         entered: {
+            type: Date, default: Date.now
+         }
+      },
+      (err, newProduct) => {
+         if (err) {
+            console.log(err);
+         } else {
+            console.log(newProduct);
+            res.json(newProduct)
+         }
+      });
+};
+
+exports.detail = (req, res) => {
+   Product.findById(req.params.id, (err, product) => {
+      if (err) {
+         console.log(err);
+      } else {
+         res.json(product)
+      }
+   });
+};
+
+exports.update = (req, res) => {
+   Product.findByIdAndUpdate(req.params.id, {}, (err, product) => {
+      if (err) {
+         console.log(err);
+      } else {
+         res.json(product)
+      }
+   });
+};
+
+exports.delete = (req, res) => {
+   Product.findByIdAndRemove(req.params.id, (err) => {
+      if (err) {
+         res.json(err);
+      } else {
+         res.json('Product has been deleted.');
+      }
+   });
+};
+
+
+
+
+
