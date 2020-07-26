2fe22ef8dd11fbe24e98a35924095f77a63e2cd6
atmavi <atmavi0001@gmail.com>
2020-07-26 11:36:56 +0800

    Created change event for login inputs

--- react-plarmy/src/components/Login.jsx
+++ react-plarmy/src/components/Login.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NavBar() {
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+
+   const handleUsernameChange = e => {
+      setUsername(e.target.value);
+   }
+
+   const handlePasswordChange = e => {
+      setUsername(e.target.value);
+   }
+
    return (
       <div className="login__container">
          <form className="login__box">
             <h1 className="">Welcome Back!</h1>
             <div className="input-group">
                <label htmlFor="login[username]" className="login__label">Username</label>
-               <input type="text" name="login[username]" id="login-username" />
+               <input
+                  type="text"
+                  name="login[username]"
+                  id="login-username"
+                  onChange={handleUsernameChange} />
             </div>
             <div className="input-group">
                <label htmlFor="login[password]" className="login__label">Password</label>
-               <input type="text" name="login[password]" id="login-password" />
+               <input
+                  type="text"
+                  name="login[password]"
+                  id="login-password"
+                  onChange={handlePasswordChange}
+               />
             </div>
             <button className="btn btn--primary login__btn d-block w-100">Login</button>
          </form>
