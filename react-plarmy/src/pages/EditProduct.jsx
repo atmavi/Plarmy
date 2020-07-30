2eb7cbdd97a2094ad1865f0e816f0f038803150b
atmavi <atmavi0001@gmail.com>
2020-07-31 04:50:19 +0800

    Applying formik to EditProduct component

--- react-plarmy/src/pages/EditProduct.jsx
+++ react-plarmy/src/pages/EditProduct.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function EditProduct() {
    return (
-      <h1>Edit Product</h1>
+      <div>
+         <h1>Any place in your app!</h1>
+         <Formik
+            initialValues={{ email: '', password: '', test: '' }}
+            validate={values => {
+               const errors = {};
+               if (!values.email) {
+                  errors.email = 'Required';
+               } else if (
+                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+               ) {
+                  errors.email = 'Invalid email address';
+               }
+               return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+               setTimeout(() => {
+                  console.log(values)
+                  setSubmitting(false);
+
+               }, 400);
+            }}
+         >
+            {({ isSubmitting }) => (
+               <Form>
+                  <Field type="text" name="email" />
+                  <ErrorMessage name="email" component="div" />
+                  <Field type="password" name="password" />
+                  <ErrorMessage name="password" component="div" />
+
+                  <Field type="text" name="test" />
+                  <ErrorMessage name="test" component="div" />
+
+                  <button type="submit" disabled={isSubmitting}>
+                     Submit
+                  </button>
+               </Form>
+            )}
+         </Formik>
+      </div>
+
    )
 }
 
 export default EditProduct;
+
+
