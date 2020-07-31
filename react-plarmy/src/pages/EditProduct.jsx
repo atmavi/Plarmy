3a5445cd202aae1883b7a1cf4bebaae103265815
atmavi <atmavi0001@gmail.com>
2020-07-31 11:10:57 +0800

    WIP-- react EditProduct component

--- react-plarmy/src/pages/EditProduct.jsx
+++ react-plarmy/src/pages/EditProduct.jsx
@@ -3,8 +3,8 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function EditProduct() {
    return (
-      <div>
-         <h1>Any place in your app!</h1>
+      <div className="edit-product">
+         <h1>Edit Product</h1>
          <Formik
             initialValues={{ email: '', password: '', test: '' }}
             validate={values => {
@@ -27,57 +27,57 @@ function EditProduct() {
             }}
          >
             {({ isSubmitting }) => (
-               <div className="edit-product">
-                  <Form className="edit-product__form">
-                     <label htmlFor="name">Name</label>
-                     <Field
-                        className="edit-product__name"
-                        name="name"
-                     />
+               <Form className="form">
+                  <label className="form__label" htmlFor="name">Name</label>
+                  <Field
+                     className="form__name"
+                     name="name"
+                  />
 
-                     <label htmlFor="price">Price</label>
-                     <Field
-                        className="edit-product__price"
-                        name="price"
-                     />
+                  <label className="form__label" htmlFor="price">Price</label>
+                  <Field
+                     className="form__price"
+                     name="price"
+                  />
 
-                     <label htmlFor="sell-volume">Selling volume</label>
-                     <Field
-                        className="edit-product__sell-volume"
-                        name="sell-volume"
-                     />
+                  <label className="form__label" htmlFor="sell-volume">Selling volume</label>
+                  <Field
+                     className="form__sell-volume"
+                     name="sell-volume"
+                  />
 
-                     <label htmlFor="image">Image</label>
-                     <Field
-                        className="edit-product__image"
-                        name="image"
-                     />
+                  <label className="form__label" htmlFor="image">Image</label>
+                  <Field
+                     className="form__image"
+                     name="image"
+                  />
 
-                     <label htmlFor="author">Author</label>
-                     <Field
-                        className="edit-product__author"
-                        name="author"
-                     />
+                  <label className="form__label" htmlFor="author">Author</label>
+                  <Field
+                     className="form__author"
+                     name="author"
+                  />
 
-                     <label htmlFor="type">type</label>
-                     <Field
-                        className="edit-product__type"
-                        name="type"
-                     />
-
+                  <label className="form__label" htmlFor="type">type</label>
+                  <Field
+                     className="form__type"
+                     name="type"
+                  />
 
 
-
-
-                     {/* <Field type="text" name="email" />
+                  {/* <Field type="text" name="email" />
                      <ErrorMessage name="email" component="div" />
                      <Field type="password" name="password" />
                      <ErrorMessage name="password" component="div" /> */}
-                     <button type="submit" disabled={isSubmitting}>
-                        Submit
-                     </button>
-                  </Form>
-               </div>
+
+                  <button
+                     type="submit"
+                     disabled={isSubmitting}
+                     className="btn btn--primary mt-1 form__btn"
+                  >
+                     Submit
+                  </button>
+               </Form>
             )}
          </Formik>
       </div >
