1ae40fa0c11dc5e1b7ab446920188795b4c5d708
atmavi <atmavi0001@gmail.com>
2020-08-03 11:40:57 +0800

    Added NotFound page

---
+++ react-plarmy/src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+   return (
+      <div className='not-found'>
+         <h1 className="not-found__header">
+            Oops...
+      </h1>
+         <div className="">404- Page not found</div>
+      </div>
+   )
+}
+
+export default NotFound;
