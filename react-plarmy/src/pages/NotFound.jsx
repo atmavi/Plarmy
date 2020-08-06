3497330ccde68c5e0d550336b07c18f5e63ae676
atmavi <atmavi0001@gmail.com>
2020-08-07 06:16:19 +0800

    Styled not-found page

--- react-plarmy/src/pages/NotFound.jsx
+++ react-plarmy/src/pages/NotFound.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
    return (
       <div className='not-found'>
-         <h1 className="not-found__header">
-            Oops...
-      </h1>
-         <div className="">404- Page not found</div>
+         <h1 className="not-found__header">Oops...</h1>
+         <div className="mt-1">We're truely sorry but the page is not found</div>
+
+         <Link to="/" className="btn btn--primary mt-3" >Go back to Home page </Link>
       </div>
    )
 }
