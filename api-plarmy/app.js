d3faa9867b63ff15faeea10f8643c604e900f910
atmavi <atmavi0001@gmail.com>
2020-09-17 12:14:54 +0800

    Added passport to api

--- api-plarmy/app.js
+++ api-plarmy/app.js
@@ -1,10 +1,42 @@
 const express = require("express"),
   mongoose = require("mongoose"),
   passport = require("passport"),
-  LocalStrategy = require("passport-local"),
-  User = require("./models/user");
+  LocalStrategy = require("passport-local");
 
 const app = express();
+
+require("dotenv").config();
+
+//MODELS
+const Product = require("./models/product");
+const User = require("./models/user");
+
+//CONTROLLERS
+const product_controller = require("./controllers/productController");
+
+//ROUTES
+const productRoutes = require("./routes/productRoutes");
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+///////////////////////////////////
+//PASSPORT CONFIG
+app.use(
+  require("express-session")({
+    secret: process.env.PASSPORT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+//////////////////////////////////
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -18,14 +50,23 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-//MODELS
-const Product = require("./models/product");
+//AUTH ROUTES
+app.post("/api/register", (req, res) => {
+  const newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
 
-//CONTROLLERS
-const product_controller = require("./controllers/productController");
+    passport.authenticate("local")(req, res, () => {
+      res.json("A new user has been addeddddd.");
+    });
+  });
+});
 
-//ROUTES
-const productRoutes = require("./routes/productRoutes");
+app.post("/api/login", passport.authenticate("local"), (req, res) => {
+  // res.send(req.user);
+});
 
 mongoose
   .connect("mongodb://localhost/api-plarmy", {
@@ -38,14 +79,10 @@ mongoose
     console.log("Connected to DB!");
   })
   .catch((err) => {
-    console.log("ERROR:" + err.message);
+    console.log("UNABLE TO CONNECT TO DB.:" + err.message);
   });
 
 app.use("/api", productRoutes);
-
-app.post("/api/login", (req, res) => {
-  console.log(req.body);
-});
 
 app.listen(4000, () => {
   console.log("App has started on port 4000");
