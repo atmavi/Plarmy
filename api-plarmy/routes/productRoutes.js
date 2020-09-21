f78e9e9bba62e6daae8a1609690ce28dfc23a848
atmavi <atmavi0001@gmail.com>
2020-09-21 18:16:59 +0800

    Add jwt middleware

--- api-plarmy/routes/productRoutes.js
+++ api-plarmy/routes/productRoutes.js
@@ -1,22 +1,35 @@
-const router = require('express').Router();
-const product_controller = require('../controllers/productController');
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
+const product_controller = require("../controllers/productController");
 
-router.get('/', product_controller.home);
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+router.get("/", product_controller.home);
 
 //GET ALL PRODUCTS
-router.get('/products', product_controller.list);
+router.get("/products", authenticateToken, product_controller.list);
 
 //CREATE PRODUCTS
-router.post('/products', product_controller.add);
+router.post("/products", product_controller.add);
 
 //SHOW A PRODUCT
-router.get('/products/:id', product_controller.detail);
+router.get("/products/:id", product_controller.detail);
 
 //UPDATE
-router.put('/products/:id', product_controller.update);
+router.put("/products/:id", product_controller.update);
 
 //DELETE
-router.delete('/products/:id', product_controller.delete);
-
+router.delete("/products/:id", product_controller.delete);
 
 module.exports = router;
